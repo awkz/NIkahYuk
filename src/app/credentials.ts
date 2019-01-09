73e8da74e73d0a47ea4b43b82565a5f57bc6c176
awkz <bararamadhan@gmail.com>
2019-01-10 02:46:43 +0700

    init

---
+++ src/app/credentials.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBIeyIB09p95hezka91UIvfb_a_V3899ZE",
+    authDomain: "belajar-4a819.firebaseapp.com",
+    databaseURL: "https://belajar-4a819.firebaseio.com",
+    projectId: "belajar-4a819",
+    storageBucket: "belajar-4a819.appspot.com",
+    messagingSenderId: "178535253331"
+  };
